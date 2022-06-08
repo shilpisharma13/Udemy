f523e3c73d339003176da48482bd5bbbc8a5c91c
shilpisharma13 <shilpi13@gmail.com>
2022-06-08 20:25:16 +0930

    commit with props

--- React/react-app/src/index.js
+++ React/react-app/src/index.js
@@ -11,40 +11,70 @@ import './index.css'
 //its better to use JSX instead of using multiple nested create elements. Below code is more readable than using JS and not JSX
 // function should be always have first letter in uppercase. Greeting is a component
 
+const books = [
+  {
+    id: 1,
+    title: "The Gruffalo's Child",
+    author: 'Julia Donaldson',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/517JtD-ak+L._SX397_BO1,204,203,200_.jpg',
+    alt: 'The Gruffalos Child',
+  },
+  {
+    id: 2,
+    title: 'Where is the GREEN Sheep?',
+    author: 'Mem Fox',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/61PvPVtGLrL._SX482_BO1,204,203,200_.jpg',
+    alt: 'Where is the GREEN Sheep?',
+  },
+  {
+    id: 3,
+    title: 'PIG the PUG',
+    author: 'Aaron Blabey',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/61E31TKCVFL._SX258_BO1,204,203,200_QL70_ML2_.jpg',
+    alt: 'PIG the PUG',
+  },
+]
+
 function BookList() {
   return (
     <section className='booklist'>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      {books.map((book) => {
+        const { img, title, author, alt } = book
+        return <Book key={book.id} book={book}></Book>
+      })}
+      {/* <Book img={firstBook.img} title={firstBook.title} author={firstBook.author} alt={firstBook.alt}>
+        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+      </Book>
+      <Book img={secondBook.img} title={firstBook.title} author={secondBook.author}alt={secondBook.alt}/> */}
     </section>
   )
 }
-
-const Book = () => {
-  const title = 'Where is the GREEN Sheepppp?'
+//Props is the object
+const Book = (props) => {
+  const { img, title, author, alt } = props.book
+  console.log(props)
   return (
     <article className='book'>
-      <Image />
+      <img src={img} alt={alt} />
       <h1>{title}</h1>
-      <Author />
+      <h2>{author}</h2>
+      {/* {children} */}
+      {/* <Author /> */}
     </article>
   )
 }
-const Image = () => {
-  return (
-    <img
-      src='https://images-na.ssl-images-amazon.com/images/I/61PvPVtGLrL._SX482_BO1,204,203,200_.jpg'
-      alt='greenSheep'
-    />
-  )
-}
+// const Image = () => {
+//   return (
+//     <img
+//       src='https://images-na.ssl-images-amazon.com/images/I/61PvPVtGLrL._SX482_BO1,204,203,200_.jpg'
+//       alt='greenSheep'
+//     />
+//   )
+// }
 
 // const Title = () => <h1>Where is the Green Sheep?</h1>
 
-const Author = () => <h4>By: Mem Fox</h4>
+// const Author = () => <h4>By: Mem Fox</h4>
 
 // ReactDom.render(<Greeting />, document.getElementById('root')) // Old way to do it
 const root = ReactDom.createRoot(document.getElementById('root'))
