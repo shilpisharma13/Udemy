6abdd97cd4e9d2ed9efd20b806b4471cbb5e9266
shilpisharma13 <shilpi13@gmail.com>
2022-06-09 20:46:16 +0930

    starter code deleted

--- React/react-advanced-2020/src/App.js
+++ React/react-advanced-2020/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import Setup from './tutorial/1-useState/setup/5-useState-counter'
+import Final from './tutorial/1-useState/final/5-useState-counter'
 
 function App() {
   return (
     <div className='container'>
-      <Setup />
+      <Final />
       {/* <h2>Advanced React Tutorial</h2> */}
     </div>
   )
