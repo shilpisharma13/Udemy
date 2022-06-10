e36cd4c122ac5b61449f5cb6a6f2c9b4b51dbeaa
shilpisharma13 <shilpi13@gmail.com>
2022-06-10 22:15:28 +0930

    tours project done

--- React/react-projects/02-tours/setup/src/App.js
+++ React/react-projects/02-tours/setup/src/App.js
@@ -8,8 +8,49 @@ function App() {
   const [loading, setLoading] = useState(true)
   const [tours, setTours] = useState([])
 
-  const getTours = async () => {}
-  return <h2>Tours Project Setup</h2>
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id)
+    setTours(newTours)
+  }
+
+  const getTours = async () => {
+    try {
+      setLoading(true)
+      const response = await fetch(url)
+      const tours = await response.json()
+      setLoading(false)
+      setTours(tours)
+    } catch (error) {
+      setLoading(true)
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    getTours()
+  }, [])
+
+  if (loading) {
+    return (
+      <>
+        <Loading />
+      </>
+    )
+  }
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className='title'>
+          <h2>No Tours left</h2>
+          <button className='btn' onClick={getTours}>
+            Refresh
+          </button>
+        </div>
+      </main>
+    )
+  }
+  return <Tours tours={tours} removeTour={removeTour} />
 }
 
 export default App
