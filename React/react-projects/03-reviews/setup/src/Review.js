5d2c9cec5048a3007fc4efcae58a128c4bd26144
shilpisharma13 <shilpi13@gmail.com>
2022-06-11 12:41:47 +0930

    React Review project added

--- React/react-projects/03-reviews/setup/src/Review.js
+++ React/react-projects/03-reviews/setup/src/Review.js
@@ -1,9 +1,64 @@
-import React, { useState } from 'react';
-import people from './data';
-import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import React, { useState } from 'react'
+import people from './data'
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa'
 
 const Review = () => {
-  return <h2>review component</h2>;
-};
+  const [index, setIndex] = useState(0)
+  const { name, job, image, text } = people[index]
 
-export default Review;
+  const checkNum = (num) => {
+    if (num > people.length - 1) return 0
+    if (num < 0) return people.length - 1
+    return num
+  }
+
+  const nextPerson = () => {
+    setIndex((index) => {
+      let newIndex = index + 1
+      return checkNum(newIndex)
+    })
+  }
+  const prevPerson = () => {
+    setIndex((index) => {
+      let newIndex = index - 1
+      return checkNum(newIndex)
+    })
+  }
+
+  const randomPerson = () => {
+    let newIndex = Math.floor(Math.random() * people.length)
+    if (newIndex === index) {
+      newIndex -= 1
+      console.log(newIndex)
+    }
+    newIndex = checkNum(newIndex)
+    return setIndex(newIndex)
+  }
+
+  return (
+    <article className='review'>
+      <div className='img-container'>
+        <img src={image} alt={name} className='person-img' />
+        <span className='quote-icon'>
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className='author'>{name}</h4>
+      <p className='job'>{job}</p>
+      <p className='info'>{text}</p>
+      <div className='button-container'>
+        <button className='prev-btn' onClick={prevPerson}>
+          <FaChevronLeft />
+        </button>
+        <button className='next-btn' onClick={nextPerson}>
+          <FaChevronRight />
+        </button>
+      </div>
+      <button className='random-btn' onClick={randomPerson}>
+        Surprise me
+      </button>
+    </article>
+  )
+}
+
+export default Review
