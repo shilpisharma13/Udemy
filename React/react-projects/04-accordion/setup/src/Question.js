97e5f23e4fd7df2e8dd8c7d6f7e9ce686eda7948
shilpisharma13 <shilpi13@gmail.com>
2022-06-11 13:11:19 +0930

    React accordian project added

--- React/react-projects/04-accordion/setup/src/Question.js
+++ React/react-projects/04-accordion/setup/src/Question.js
@@ -1,7 +1,20 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
-};
+import React, { useState } from 'react'
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai'
+const Question = (question) => {
+  const { id, title, info } = question
+  const [showInfo, setShowInfo] = useState(false)
 
-export default Question;
+  return (
+    <article className='question'>
+      <header>
+        <h4>{title}</h4>
+        <button className='btn' onClick={() => setShowInfo(!showInfo)}>
+          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {showInfo && <p>{info}</p>}
+    </article>
+  )
+}
+
+export default Question
