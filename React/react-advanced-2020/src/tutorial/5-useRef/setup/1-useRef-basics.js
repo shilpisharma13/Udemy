f80800695d200c5a9a488c90137f121848e89219
shilpisharma13 <shilpi13@gmail.com>
2022-07-02 12:23:58 +0930

    useRef initiated

--- React/react-advanced-2020/src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ React/react-advanced-2020/src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -1,11 +1,26 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react'
 
 // preserves value
 // DOES NOT trigger re-render
 // target DOM nodes/elements
 
 const UseRefBasics = () => {
-  return <h2>useRef</h2>;
-};
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+  return (
+    <>
+      <form
+        action='
+  div'
+        className='form'
+        onSubmit={handleSubmit}
+      >
+        <input type='text' />
+        <button type='button'>Submit</button>
+      </form>
+    </>
+  )
+}
 
-export default UseRefBasics;
+export default UseRefBasics
