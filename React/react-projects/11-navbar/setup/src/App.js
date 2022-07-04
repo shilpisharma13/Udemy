aa8155edebca7c1c320bfd79be6ddf13385ab58f
shilpisharma13 <shilpi13@gmail.com>
2022-07-04 20:28:26 +0930

    navbar project done

---
+++ React/react-projects/11-navbar/setup/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Navbar'
+import logo from './logo.svg'
+function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default App
