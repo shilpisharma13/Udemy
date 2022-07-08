8055d24b0816a63f583287417ba5fb7e0f5dd4e8
shilpisharma13 <shilpi13@gmail.com>
2022-07-08 10:32:51 +0930

    useReducer added

--- React/react-advanced-2020/src/tutorial/6-useReducer/setup/index.js
+++ React/react-advanced-2020/src/tutorial/6-useReducer/setup/index.js
@@ -1,10 +1,53 @@
-import React, { useState, useReducer } from 'react';
-import Modal from './Modal';
-import { data } from '../../../data';
+import React, { useState, useReducer } from 'react'
+import Modal from './Modal'
+import { data } from '../../../data'
 // reducer function
 
-const Index = () => {
-  return <h2>useReducer</h2>;
-};
+const reducer = (state, action) => {
+  if (action.type === 'ADD_ITEM') {
+    return { ...state, people: data, isModalOpen: true, modalContent: 'hello' }
+  }
+}
 
-export default Index;
+const defaultState = {
+  people: [],
+  isModalOpen: false,
+  modalContent: '',
+}
+
+const Index = () => {
+  const [name, setName] = useState('')
+  const [state, dispatch] = useReducer(reducer, defaultState)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (name) {
+      dispatch({ type: 'ADD_ITEM' })
+    } else {
+    }
+  }
+  return (
+    <>
+      {defaultState.isModalOpen && <Modal modalContent={state.modalContent} />}
+      <form className='form' onSubmit={handleSubmit}>
+        <div>
+          <input
+            type='text'
+            name='name'
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <button>Submit</button>
+      </form>
+      {defaultState.people.map((person) => {
+        return (
+          <div key={person.id}>
+            <h4>{person.name}</h4>
+          </div>
+        )
+      })}
+    </>
+  )
+}
+
+export default Index
