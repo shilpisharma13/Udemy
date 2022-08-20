7e9b5647eb669ac4c2e1fddba26f8292e744305c
shilpisharma13 <shilpi13@gmail.com>
2022-08-20 20:50:39 +0930

    node js first project in progress

--- Node JS/03-task-manager/starter/db/connect.js
+++ Node JS/03-task-manager/starter/db/connect.js
@@ -1,14 +1,12 @@
 const mongoose = require('mongoose')
 
-const connectionString =
-  'mongodb+srv://shilpi13:yuviyudi1830@cluster0.zqyb4xm.mongodb.net/TASK-MANAGER?retryWrites=true&w=majority'
-
-mongoose
-  .connect(connectionString, {
+const connectDB = (url) => {
+  return mongoose.connect(url, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
   })
-  .then(() => console.log('CONNECTED TO DB...'))
-  .catch((err) => console.log(err))
+}
+
+module.exports = connectDB
