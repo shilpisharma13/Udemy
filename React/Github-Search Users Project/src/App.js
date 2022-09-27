a684eb7b2aaad4afdf20c15a5a98f46764675e65
shilpisharma13 <shilpi13@gmail.com>
2022-09-27 12:26:09 +0930

    react github project initiated

---
+++ React/Github-Search Users Project/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Dashboard />} />
+        <Route path='login' element={<Login />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+      {/* <Route path='login' element={<Login setUser={setUser}></Login>} />
+          <Route
+            path='dashboard'
+            element={
+              <ProtectedRoute user={user}>
+                <Dashboard user={user} />
+              </ProtectedRoute>
+            }
+          /> */}
+    </Router>
+  )
+}
+
+export default App
