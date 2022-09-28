64086bf08c1c17114a2d5e438b54ae2961453084
shilpisharma13 <shilpi13@gmail.com>
2022-09-28 11:56:52 +0930

    React github search user project added charts

--- React/Github Search User Project/src/components/Charts/Doughnut2d.js
+++ React/Github Search User Project/src/components/Charts/Doughnut2d.js
@@ -1,7 +1,47 @@
-import React from 'react';
+// STEP 1 - Include Dependencies
+// Include react
+import React from 'react'
+// Include the react-fusioncharts component
+import ReactFC from 'react-fusioncharts'
+// Include the fusioncharts library
+import FusionCharts from 'fusioncharts'
+// Include the chart type
+import Chart from 'fusioncharts/fusioncharts.charts'
+// Include the theme as fusion
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.candy'
+// Adding the chart and theme as dependency to the core fusioncharts
+ReactFC.fcRoot(FusionCharts, Chart, FusionTheme)
 
-const Doughnut2d = () => {
-  return <div>chart</div>;
-};
+const ChartComponent = ({ data }) => {
+  const chartConfigs = {
+    type: 'doughnut2d', // The chart type
+    width: '100%', // Width of the chart
+    height: '400', // Height of the chart
+    dataFormat: 'json', // Data type
+    dataSource: {
+      // Chart Configuration
+      chart: {
+        //Set the chart caption
+        caption: 'Stars per language',
+        //Set the chart subcaption
+        // subCaption: 'In MMbbl = One Million barrels',
+        // //Set the x-axis name
+        // xAxisName: 'Country',
+        // //Set the y-axis name
+        // yAxisName: 'Reserves (MMbbl)',
+        // numberSuffix: 'K',
+        //Set the theme for your chart
+        theme: 'candy',
+        decimals: 0,
+        doughnutRadius: '35%',
+        showPercentValues: 0,
+        // paletteColors: '#f0db4f, #fe4532, #35ff456',
+      },
+      // Chart Data
+      data,
+    },
+  }
+  return <ReactFC {...chartConfigs} />
+}
 
-export default Doughnut2d;
+export default ChartComponent
--- React/Github Search User Project/src/context/context.js
+++ React/Github Search User Project/src/context/context.js
@@ -1,7 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import mockUser from './mockData.js/mockUser';
-import mockRepos from './mockData.js/mockRepos';
-import mockFollowers from './mockData.js/mockFollowers';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react'
+import mockUser from './mockData.js/mockUser'
+import mockRepos from './mockData.js/mockRepos'
+import mockFollowers from './mockData.js/mockFollowers'
+import axios from 'axios'
+import { useContext } from 'react'
 
-const rootUrl = 'https://api.github.com';
+const rootUrl = 'https://api.github.com'
+
+const GithubContext = React.createContext()
+const useGlobalContext = () => {
+  return useContext(GithubContext)
+}
+
+//We now have access to Provider and Consumer
+
+const GithubProvider = ({ children }) => {
+  const [githubUser, setGithubUser] = useState(mockUser)
+  const [repos, setRepos] = useState(mockRepos)
+  const [followers, setFollowrs] = useState(mockFollowers)
+
+  return (
+    <GithubContext.Provider value={{ githubUser, repos, followers }}>
+      {children}
+    </GithubContext.Provider>
+  )
+}
+
+export { GithubProvider, useGlobalContext }
