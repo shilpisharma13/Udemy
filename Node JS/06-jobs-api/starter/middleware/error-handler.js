3e35d180a29b50c89b83e17fb12a4adbe4b9bcc6
shilpisharma13 <shilpi13@gmail.com>
2022-10-19 09:15:47 +1030

    node js project

--- Node JS/06-jobs-api/starter/middleware/error-handler.js
+++ Node JS/06-jobs-api/starter/middleware/error-handler.js
@@ -1,10 +1,32 @@
-const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
+  console.log(err.message)
+  let customError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || 'Something went wrong, try again later',
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+
+  if (err.name === 'ValidationError') {
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(', ')
+    customError.statusCode = 400
+  }
+
+  if (err.code && err.code === 11000) {
+    customError.msg = `Account already exist for ${Object.values(
+      err.keyValue
+    )}. Please try logging in`
+    customError.statusCode = 400
+  }
+
+  if (err.name === 'CastError') {
+    customError.msg = `Invalid job id ${err.value}`
+    customError.statusCode = StatusCodes.NOT_FOUND
+  }
+
+  return res.status(customError.statusCode).json({ msg: customError.msg })
+  // return res.status(StatusCodes.BAD_REQUEST).json({ err })
 }
 
 module.exports = errorHandlerMiddleware
