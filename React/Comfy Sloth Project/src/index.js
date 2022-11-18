7cf4117809b5f8b84302db7235a439da7b649c70
shilpisharma13 <shilpi13@gmail.com>
2022-11-18 13:37:15 +1030

    comfy sloth project cart functionality added

--- React/Comfy Sloth Project/src/index.js
+++ React/Comfy Sloth Project/src/index.js
@@ -6,13 +6,24 @@ import { ProductsProvider } from './context/products_context'
 import { FilterProvider } from './context/filter_context'
 import { CartProvider } from './context/cart_context'
 import { UserProvider } from './context/user_context'
-import { Auth0Provider } from '@auth0/auth0-react'
+import { Auth0Provider, User } from '@auth0/auth0-react'
 
 ReactDOM.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <App />
-    </FilterProvider>
-  </ProductsProvider>,
+  <Auth0Provider
+    domain='dev-cmsfd82m.us.auth0.com'
+    clientId='YW9NlzLKzpfGlAzG04C0Wz9Vo2gDXe8W8'
+    redirectUri={window.location.origin}
+    cacheLocation='localstorage'
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>,
   document.getElementById('root')
 )
--- React/Comfy Sloth Project/src/reducers/cart_reducer.js
+++ React/Comfy Sloth Project/src/reducers/cart_reducer.js
@@ -7,7 +7,80 @@ import {
 } from '../actions'
 
 const cart_reducer = (state, action) => {
-  return state
+  if (action.type === ADD_TO_CART) {
+    const { id, color, amount, product } = action.payload
+    const tempItem = state.cart.find((i) => i.id === id + color)
+    if (tempItem) {
+      const tempCart = state.cart.map((cartItem) => {
+        if (cartItem.id === id + color) {
+          let newAmount = cartItem.amount + amount
+          if (newAmount > cartItem.max) {
+            newAmount = cartItem.max
+          }
+          return { ...cartItem, amount: newAmount }
+        } else {
+          return cartItem
+        }
+      })
+
+      return { ...state, cart: tempCart }
+    } else {
+      const newItem = {
+        id: id + color,
+        name: product.name,
+        color,
+        amount,
+        image: product.images[0].url,
+        price: product.price,
+        max: product.stock,
+      }
+      return { ...state, cart: [...state.cart, newItem] }
+    }
+  }
+  if (action.type === REMOVE_CART_ITEM) {
+    const tempCart = state.cart.filter((item) => item.id !== action.payload)
+    return { ...state, cart: tempCart }
+  }
+  if (action.type === CLEAR_CART) {
+    return { ...state, cart: [] }
+  }
+  if (action.type === TOGGLE_CART_ITEM_AMOUNT) {
+    const { id, value } = action.payload
+    const tempCart = state.cart.map((item) => {
+      if (item.id === id) {
+        if (value === 'inc') {
+          let newAmount = item.amount + 1
+          if (newAmount > item.max) {
+            newAmount = item.max
+          }
+          return { ...item, amount: newAmount }
+        }
+        if (value === 'dec') {
+          let newAmount = item.amount - 1
+          if (newAmount < 1) {
+            newAmount = 1
+          }
+          return { ...item, amount: newAmount }
+        }
+      } else {
+        return item
+      }
+    })
+    return { ...state, cart: tempCart }
+  }
+  if (action.type === COUNT_CART_TOTALS) {
+    let items = 0,
+      total = 0
+    state.cart.forEach((item) => {
+      items = items + item.amount
+      return items
+    })
+    state.cart.forEach((item) => {
+      total = total + item.price * item.amount
+      return total
+    })
+    return { ...state, total_items: items, total_amount: total }
+  }
   throw new Error(`No Matching "${action.type}" - action type`)
 }
 
