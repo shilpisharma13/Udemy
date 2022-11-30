f4c1832fa601a16338764f286000dfb879d95fa1
shilpisharma13 <shilpi13@gmail.com>
2022-11-30 12:38:43 +1030

    redux done

--- React/redux-toolkit/starter/src/features/cart/cartSlice.js
+++ React/redux-toolkit/starter/src/features/cart/cartSlice.js
@@ -75,7 +75,6 @@ const cartSlice = createSlice({
         state.isLoading = true
       })
       .addCase(getCartItems.fulfilled, (state, action) => {
-        console.log(action)
         state.isLoading = false
         state.cartItems = action.payload
       })
